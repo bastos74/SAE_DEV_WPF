3c1df287fd8521c6ececcc84f758e3ed8a652686
Hugo Clerc-Renaud <Hugo.Clerc-Renaud@etu.univ-savoie.fr>
2023-06-14 16:00:53 +0200

    debut categorie dans materiel

--- SAE_DEV_WPF/Model/ApplicationData.cs
+++ SAE_DEV_WPF/Model/ApplicationData.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace SAE_DEV_WPF.Model
@@ -24,6 +25,18 @@ namespace SAE_DEV_WPF.Model
 
             Materiel m = new Materiel();
             LesMateriels = m.FindAll();
+
+            // pour chaque materiel, on lui affecte son groupe
+            foreach (Materiel mat in LesMateriels.ToList())
+            {
+                mat.Categorie = LesCategorie.ToList().Find(g => g.Id == unEtud.FK_IdGroupe);
+            }
+            // pour chaque categorie, on affecte toutes les références des matériel appartenant au groupe
+            foreach (Groupe unGroupe in LesGroupes.ToList())
+            {
+                unGroupe.LesEtudiants = new ObservableCollection<Etudiant>(
+                LesEtudiants.ToList().FindAll(e => e.FK_IdGroupe == unGroupe.Id));
+            }
         }
     }
 }
