ae67e63e13cc0c678910d694c03464751b1acd2a
Nicolas Tochon <Nicolas.Tochon@etu.univ-smb.fr>
2023-06-19 17:59:51 +0200

    a

--- SAE_DEV_WPF/WinAttribution.xaml.cs
+++ SAE_DEV_WPF/WinAttribution.xaml.cs
@@ -123,7 +123,7 @@ namespace SAE_DEV_WPF
             applicationData.LesAttributions.Last().FindAll(); // tentative d'actualisation
             dgAttribution.Items.Refresh();
 
-            
+            dgAttribution.Items.Refresh();
             ((Button)sender).Background = Util.GetBaseColor();
         }
 
